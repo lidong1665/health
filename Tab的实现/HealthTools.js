2d7555db8c5e2d0936930ae6d4c5745feb51fe59
lidong1665 <lidong1665@163.com>
2016-02-29 09:43:04 +0800

    初次导入项目

---
+++ Tab的实现/HealthTools.js
@@ -0,0 +1,149 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+class HealthTools extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+       <View style={styles.view}>
+        <Image source={require('./image/tool_lxtz.png')}
+          style={styles.imageIcon} />
+         <View style={styles.view2}>
+         <Text style={styles.t0}>理想体重计算器</Text>
+         <Text style={styles.t1}>理想体重指的是体重数值在正常范围内，构成比例...</Text>
+         </View>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/tool_bmi.png')}
+          style={styles.imageIcon} />
+         <View style={styles.view2}>
+         <Text style={styles.t0}>体重指数BMI</Text>
+         <Text style={styles.t1}>想知道您的体重是否超标吗？体重指数(BMI)能反应...</Text>
+         </View>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/tool_ytb.png')}
+          style={styles.imageIcon} />
+         <View style={styles.view2}>
+         <Text style={styles.t0}>腰臀比计算</Text>
+         <Text style={styles.t1}>想知道您的体重是否超标吗？体重指数(BMI)能反应...</Text>
+         </View>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/tool_nlxq.png')}
+          style={styles.imageIcon} />
+         <View style={styles.view2}>
+         <Text style={styles.t0}>每日能量需求</Text>
+         <Text style={styles.t1}>想知道您一天究竟该摄入多少能量吗？根据您提供...</Text>
+         </View>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/tool_yys.png')}
+          style={styles.imageIcon} />
+         <View style={styles.view2}>
+         <Text style={styles.t0}>营养素计算器</Text>
+         <Text style={styles.t1}>每种食物均含有不同的营养素，想了解您摄入食物中...</Text>
+         </View>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/tool_j.png')}
+          style={styles.imageIcon} />
+         <View style={styles.view2}>
+         <Text style={styles.t0}>减掉一公斤</Text>
+         <Text style={styles.t1}>不同的运动锻炼方法，能量消耗的数量和方式也不相...  </Text>
+         </View>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/tool_ydnh.png')}
+          style={styles.imageIcon} />
+         <View style={styles.view2}>
+         <Text style={styles.t0}>运动能耗计算</Text>
+         <Text style={styles.t1}>用食物重量图谱可以帮助你更加直观的了解各种常用...   </Text>
+         </View>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#F5FCFF',
+  },
+  view:{
+    flexDirection:'row',
+    alignItems: 'stretch',
+    height:70,
+    alignSelf:'stretch',
+    backgroundColor: '#F8F8FF',
+  },
+  line:{
+   height:1,
+   alignSelf:'stretch',
+   backgroundColor: '#708090',
+  },
+  imageIcon:{
+    height:50,
+    width:50,
+    alignSelf:'center',
+    marginLeft:20,
+    marginRight:20,
+  },
+  t0:{
+    fontSize:20,
+  },
+  t1:{
+    fontSize:10,
+  },
+
+  imageArr:{
+    height:0,
+    width:0,
+    alignSelf:'center',
+    marginLeft:10,
+  },
+  view2:{
+    flexDirection:'column',
+    height:50,
+    alignSelf:'center',
+    backgroundColor: '#F8F8FF',
+  },
+});
+
+module.exports = HealthTools;
---
+++ Tab的实现/MyHealth.js
@@ -0,0 +1,115 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+class MyHealth extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      
+        <View style={styles.view}>
+        <Image source={require('./image/base_health.png')}
+        style={styles.imageIcon} />
+        <Text style={styles.t0}>健康指标</Text>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+        <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/health_test.png')}
+        style={styles.imageIcon} />
+        <Text style={styles.t0}>健康监测</Text>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+         <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/health_report.png')}
+        style={styles.imageIcon} />
+        <Text style={styles.t0}>健康报告</Text>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+         <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/base_log.png')}
+        style={styles.imageIcon} />
+        <Text style={styles.t0}>健康日志</Text>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+         <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/health_manage_task.png')}
+        style={styles.imageIcon} />
+        <Text style={styles.t0}>健康任务</Text>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+         <View style={styles.line}/>
+        <View style={styles.view}>
+        <Image source={require('./image/health_manage.png')}
+        style={styles.imageIcon} />
+        <Text style={styles.t0}>健康目标</Text>
+         <Image source={require('./image/arrows_right.png')}
+         style={styles.imageArr} />
+        </View>
+         <View style={styles.line}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#F5FCFF',
+  },
+  view:{
+    flexDirection:'row',
+    alignItems: 'stretch',
+    height:50,
+    alignSelf:'stretch',
+    backgroundColor: '#F8F8FF',
+  },
+  line:{
+   height:1,
+   alignSelf:'stretch',
+   backgroundColor: '#708090',
+  },
+  imageIcon:{
+    height:30,
+    width:30,
+    alignSelf:'center',
+    marginLeft:20,
+    marginRight:20,
+  },
+  t0:{
+    alignSelf:'center',
+    fontSize:20,
+  },
+
+  imageArr:{
+    height:20,
+    width:20,
+    alignSelf:'center',
+    marginLeft:120,
+  },
+
+
+});
+
+module.exports = MyHealth;
